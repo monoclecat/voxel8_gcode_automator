c3b0c0f66a9a0ce628a4d75185239320cd8d32d8
Andy Delay <signup@andrewdelay.com>
2018-09-05 12:11:44 +0200

    renamed script and added gcode files to gitignore

--- voxel8_gcode_automator.py
+++ voxel8_gcode_automator.py
@@ -1,12 +1,15 @@
 import sys, re
 g1_xy_code_with_speed = re.compile('G1 X(?P<x_coord>(\d+\.\d+)) Y(?P<y_coord>(\d+\.\d+)) F(?P<speed>(\d+\.\d+))')
-g1_no_xy_with_speed = re.compile('G1 (?P<number>(\d+\.\d+)) F(?P<speed>(\d+\.\d+))')
+# g1_no_xy_with_speed = re.compile('G1 (?P<number>(\d+\.\d+)) F(?P<speed>(\d+\.\d+))')
 turn_off_temp = re.compile('M104 S0.*')
 
 MAX_SPEED = 800.0
 
+if len(sys.argv) != 2 or not sys.argv[1].find('.gcode'):
+    print("Usage: python voxel8_gcode_automator.py file_to_modify.gcode")
+
 file = open(sys.argv[1], 'r')
-output = open('output.gcode', 'w')
+output = open('mod_'+sys.argv[1], 'w')
 pre_code = open('pre_gcode_snippet.gcode', 'r')
 post_code = open('post_gcode_snippet.gcode', 'r')
 
