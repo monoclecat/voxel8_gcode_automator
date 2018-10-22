66401cc201a8bcfa3b790d4398c9c1cf5544a659
Andy Delay <signup@andrewdelay.com>
2018-10-22 14:23:48 +0200

    Output is now buffered before being written to file Added G92 command at beginning of output file

--- voxel8_gcode_adjuster.py
+++ voxel8_gcode_adjuster.py
@@ -1,6 +1,7 @@
 import sys, re, logging
 from print_in_color import *
 
+g1_command = re.compile('G1 X(?P<x_home>\d+\.\d+) Y(?P<y_home>\d+\.\d+)')
 
 def print_help():
     print_blue("Display this help text: python voxel8_gcode_adjuster.py help")
@@ -22,22 +23,37 @@ file_path = sys.argv[1][:sys.argv[1].rfind('/')+1]
 output = open(file_path+'mod_'+file_name, 'w')
 
 pressure = input(bcolors.OKGREEN+"What should the extrusion pressure be? Numbers only! -> "+bcolors.ENDC)
-output.write(';converted using the Voxel8 gcode adjuster tool\n'
-             'G90\n'
-             'G21\n'
-             'M125 S{}		;fill pressure tank\n'
-             'M236 S{}      ;set extrusion pressure\n'
-             'T1			;choose silver cartridge\n'
-             'M42 P2 S255		;open valve T1\n'
-             'G4 P700\n'
-             'G1 F500\n'
-             '; End of the preamble inserted by the Voxel8 gcode adjuster tool\n\n\n'.format(str(int(pressure)+20), pressure)
-             )
+buffer = [';converted using the Voxel8 gcode adjuster tool\n',  # Line 0
+          'G90\n',  # Line 1
+          '; This line shall be replaced with the G92 command',  # Line 2
+          'G21\n',
+          'M125 S{}	       ;fill pressure tank\n'.format(str(int(pressure)+20)),
+          'M236 S{}        ;set extrusion pressure\n'.format(pressure),
+          'T1              ;choose silver cartridge\n',
+          'M42 P2 S255     ;open valve T1\n',
+          'G4 P700\n',
+          'G1 F500\n',
+          '; End of the preamble inserted by the Voxel8 gcode adjuster tool\n\n\n']
+
+# output.write(';converted using the Voxel8 gcode adjuster tool\n'  # Line 0
+#              'G90\n'  # Line 1
+#              '; This line shall be replaced with the G92 command'  # Line 2
+#              'G21\n'
+#              'M125 S{}		;fill pressure tank\n'
+#              'M236 S{}      ;set extrusion pressure\n'
+#              'T1			;choose silver cartridge\n'
+#              'M42 P2 S255		;open valve T1\n'
+#              'G4 P700\n'
+#              'G1 F500\n'
+#              '; End of the preamble inserted by the Voxel8 gcode adjuster tool\n\n\n'.format(str(int(pressure)+20), pressure)
+#              )
 
 copy_line = False
 
 beginning_of_source_file_trimmed = False
 extrusion_preamble_comment = '; Extrusion Preamble'
+
+find_home = False
 
 inside_mid_layer_wipe = False
 mid_layer_wipe_comment = '; Mid-layer wipe'
@@ -46,19 +62,25 @@ end_of_source_file_trimmed = False
 shut_off_heaters_comment = '; shut off heaters'
 
 for i, line in enumerate(file):
-    # Delete all lines up to the first G1 command after '; Extrusion Preamble' comment
     if not beginning_of_source_file_trimmed:
         if line.find(extrusion_preamble_comment) == 0:
             print_blue("Starting copying lines of source file starting line {}".format(i+1))
             copy_line = True
             beginning_of_source_file_trimmed = True
+            find_home = True
+
+    if find_home:
+        match = g1_command.match(line)
+        if match:
+            set_home_cmd = 'G92 X{} Y{} Z0.0'.format(match.group('x_home'), match.group('y_home'))
+            buffer[2] = set_home_cmd
 
     if not inside_mid_layer_wipe:
         if line.find(mid_layer_wipe_comment) == 0:
             print_blue("Mid-layer wipe found in line {}".format(i+1))
             copy_line = False
             inside_mid_layer_wipe = True
-            output.write('\n\n\n; A mid-layer wipe was removed here \n\n\n')
+            buffer.append('\n\n\n; A mid-layer wipe was removed here \n\n\n')
     else:
         if line.find(extrusion_preamble_comment) == 0:
             print_blue("Mid-layer wipe ended in line {}".format(i+1))
@@ -72,13 +94,16 @@ for i, line in enumerate(file):
             break
 
     if copy_line:
-        output.write(line)
+        buffer.append(line)
 
-output.write('\n\n\n; Start of postamble inserted by the Voxel8 gcode adjuster tool\n\n\n'
-             'M400\n'
-             'M236 S0\n'
-             'M42 P2 S0\n'
-             'G91\n'
-             'G1 Z10 F5400\n')
+
+buffer += ['\n\n\n; Start of postamble inserted by the Voxel8 gcode adjuster tool\n\n\n',
+           'M400\n',
+           'M236 S0\n',
+           'M42 P2 S0\n',
+           'G91\n',
+           'G1 Z10 F5400\n']
+
+output.writelines(buffer)
 
 print_green('Finished! ^__^')
