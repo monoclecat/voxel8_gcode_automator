a0eb09716a4da8a8517d5a165e6196d4299e1168
Andy Delay <signup@andrewdelay.com>
2018-10-23 16:19:56 +0200

    Removed fancy colors because they don't work on windows systems. Made script compatible with Windows.

--- voxel8_gcode_adjuster.py
+++ voxel8_gcode_adjuster.py
@@ -1,32 +1,24 @@
-import sys, re, logging
-from print_in_color import *
+import sys, re, os
 
 g1_command = re.compile('G1 X(?P<x_home>\d+\.\d+) Y(?P<y_home>\d+\.\d+)')
 
+if len(sys.argv) <= 1:
+    # No command line arguments are given 
+    file_name_argument = input("Bitte ziehe die zu konvertierende Datei hier hinein -> ")
+else:
+    file_name_argument = sys.argv[1]
 
-def print_help():
-    print_blue("Display this help text: python voxel8_gcode_adjuster.py help")
-    print_blue("Usage: python voxel8_gcode_adjuster.py file_to_modify.gcode")
-
-
-if sys.argv[1] == 'help':
-    print_help()
+path, file_name = os.path.split(file_name_argument)
+if file_name.find('.gcode') == -1:
+    print("File must have a .gcode ending!")
+    input("Hit enter to close")
     exit()
 
-if sys.argv[1].find('.gcode') == -1:
-    print_help()
-    exit()
 
-file = open(sys.argv[1], 'r')
-file_name = sys.argv[1][sys.argv[1].rfind('/')+1:]
-file_path = sys.argv[1][:sys.argv[1].rfind('/')+1]
-
-output = open(file_path+'mod_'+file_name, 'w')
-
-pressure = input(bcolors.OKGREEN+"What should the extrusion pressure be? Numbers only! -> "+bcolors.ENDC)
+pressure = input("What should the extrusion pressure be? Numbers only! -> ")
 buffer = [';converted using the Voxel8 gcode adjuster tool\n',  # Line 0
           'G90\n',  # Line 1
-          '; This line shall be replaced with the G92 command',  # Line 2
+          '; This line shall be replaced with the G92 command\n',  # Line 2
           'G21\n',
           'M125 S{}	       ;fill pressure tank\n'.format(str(int(pressure)+20)),
           'M236 S{}        ;set extrusion pressure\n'.format(pressure),
@@ -36,6 +28,10 @@ buffer = [';converted using the Voxel8 gcode adjuster tool\n',  # Line 0
           'G1 F500\n',
           '; End of the preamble inserted by the Voxel8 gcode adjuster tool\n\n\n']
 
+file = open(file_name_argument, 'r')
+
+output_file_name = os.path.join(path, 'mod_'+file_name)
+output = open(output_file_name, 'w')
 
 copy_line = False
 
@@ -53,7 +49,7 @@ shut_off_heaters_comment = '; shut off heaters'
 for i, line in enumerate(file):
     if not beginning_of_source_file_trimmed:
         if line.find(extrusion_preamble_comment) == 0:
-            print_blue("Starting copying lines of source file starting line {}".format(i+1))
+            print("Starting copying lines of source file starting line {}".format(i+1))
             copy_line = True
             beginning_of_source_file_trimmed = True
             find_home = True
@@ -66,19 +62,18 @@ for i, line in enumerate(file):
 
     if not inside_mid_layer_wipe:
         if line.find(mid_layer_wipe_comment) == 0:
-            print_blue("Mid-layer wipe found in line {}".format(i+1))
+            print("Mid-layer wipe found in line {}".format(i+1))
             copy_line = False
             inside_mid_layer_wipe = True
             buffer.append('\n\n\n; A mid-layer wipe was removed here \n\n\n')
     else:
         if line.find(extrusion_preamble_comment) == 0:
-            print_blue("Mid-layer wipe ended in line {}".format(i+1))
             copy_line = True
             inside_mid_layer_wipe = False
 
     if not end_of_source_file_trimmed:
         if line.find(shut_off_heaters_comment) == 0:
-            print_blue("Stopped copying lines of source file starting line {}".format(i+1))
+            print("Stopped copying lines of source file starting line {}".format(i+1))
             copy_line = False
             break
 
@@ -95,4 +90,4 @@ buffer += ['\n\n\n; Start of postamble inserted by the Voxel8 gcode adjuster too
 
 output.writelines(buffer)
 
-print_green('Finished! ^__^')
+print('Finished! ^__^')
